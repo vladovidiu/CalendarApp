93cf67932a7f410398f2dc209ed080950bb90e74
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2014-06-20 15:13:38 +0100

    Add blankIfNegative filter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,28 @@ angular.module('calendarApp.controllers', [])
         $scope.today = new Date();
         $scope.name = "Vlad Tutunea";
     }])
-    .controller('DashboardController', ['$scope', function($scope) {
+    .controller('DashboardController', ['$scope', '$parse', function($scope, $parse) {
+        $scope.entryInput = undefined;
+
+        // Setting up watch expression to watch the entryInput
+        $scope.$watch('entryInput', function(newVal, oldVal, scope) {
+            if (newVal !== oldVal) {
+                // Look for any part of the string that starts with @
+                var strUsers = newVal.match(/[@]+[A-Za-z0-9_]+/g),
+                    i;
+                if (strUsers) {
+                    for (i = 0; i < strUsers.length; i++) {
+                        var user = strUsers[i],
+                            cleanUser = user.slice(1),
+                            parsedUser = $parse("users." + cleanUser)(scope);
+                        if (parsedUser) {
+                            // An user was found on our scope in the "users" object
+                        } else {
+                            // No user was found on our scope in the "users" object
+                        }
+                    }
+                }
+            }
+        });
 
     }]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,10 @@
-angular.module('calendarApp.filters', []);
+angular.module('calendarApp.filters', [])
+    .filter('blankIfNegative', function() {
+        return function(input) {
+            if (input <= 0) {
+                return ' ';
+            } else {
+                return input;
+            }
+        }
+    });
